8d56b7108cc753f13349b31c2dd8baf6804c36bd
koheihida <88849254+koheihida@users.noreply.github.com>
2022-01-08 23:46:32 +0900

    /example の表示のためのroute設定

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExampleController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/example', [ExampleController::class, 'index'])->name('index');
