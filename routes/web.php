70ba92d3f29f6c5dd5472606c30fe045ab47c854
koheihida <88849254+koheihida@users.noreply.github.com>
2022-01-09 15:21:11 +0900

    SkillControllerへのルーティング実装

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::middleware(['auth'])->prefix('skill')->group(function() {
+    Route::get('/', [SkillController::class, 'index'])->name('skill');
+});
+
 require __DIR__.'/auth.php';
